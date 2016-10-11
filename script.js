2309ca09f3b95deb64df6bb21c1ebfae9f4bb180
Steve Larsen <stevelarsen01@gmail.com>
2016-10-11 17:56:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+avengersMovie = {
+    name: "Avengers",
+    runtime: 143,
+    releaseYear: 2012,
+    director: "Joss Whedon"
+};
+
+console.log(avengersMovie);
+
+document.getElementById("solution").innerHTML = JSON.stringify(avengersMovie);
